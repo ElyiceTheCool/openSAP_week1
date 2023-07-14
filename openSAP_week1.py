7ea71c619af8f85f59df74498cb1d28cb7b60975
Alyssia Bunts <alyssiabunts@gmail.com>
2023-07-14 08:31:06 -0400

    finished exercise example

--- openSAP_week1.py
+++ openSAP_week1.py
@@ -1,4 +1,6 @@
 name = input("Please enter your name. ")
-start_city = input("Which city would you like tot start in? ")
+start_city = input("Which city would you like to start in? ")
 destination_city = input("Which city would you like to end in? ")
-transport = input("HOW would you like to get there? ")
+transport = input("How would you like to get there? ")
+
+print(name, "would like to travel from", start_city, "to", destination_city, f"by way of {transport}!")
