e9a6464d8f94b29a41361510fd24117a26c1106c
Alyssia Bunts <alyssiabunts@gmail.com>
2023-07-14 08:24:26 -0400

    Ininitial upload

---
+++ openSAP_week1.py
@@ -0,0 +1,4 @@
+name = input("Please enter your name. ")
+start_city = input("Which city would you like tot start in? ")
+destination_city = input("Which city would you like to end in? ")
+transport = input("HOW would you like to get there? ")
